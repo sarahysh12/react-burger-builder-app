e8e3a783e21d7595eeb8256b7a9c7e5c813db80f
sarahysh12 <saraysh22@gmail.com>
2020-03-28 12:57:10 -0700

    add logo

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientsummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
             <Button clicked={props.purchaseCancelled} btnType="Danger"> CANCEL</Button>
             <Button clicked={props.purchaseContinued} btnType="Success"> CONTINUE</Button>
