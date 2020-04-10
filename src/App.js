9c36216e6a9dfba6ff19182a1b43799f9035f76b
sarahysh12 <saraysh22@gmail.com>
2020-04-09 18:46:04 -0700

    add checkout and routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Layout>
-         <BurgerBuilder/>
-         <BurgerBuilder/>
+         <Switch>
+            <Route path="/checkout" component={Checkout}/>
+            <Route path="/orders" component={Orders}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+          
+         </Switch>
        </Layout>
       </div>
     );
